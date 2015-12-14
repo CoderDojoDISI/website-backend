e5f5eccb1e415bf2316acc95e9c502796a05c69c
mion00 <mion00@gmail.com>
2015-12-14 17:51:53 +0100

    AGGIUNTA TECNOLOGIA A CONTAINER DI DOCKER
    Aggiunto docker-compose.yml per utilizzare più di un container insieme (backend, frontend, DB, serverAPI e server Nginx)
    Aggiunti Dockerfile per frontend e backend
    Definite le dipendenze per frontend usando NPM
    Aggiunto compilatore Sass e Gulp runner per buildare frontend

--- settings.py
+++ settings.py
@@ -1,11 +1,12 @@
 import os
 
-from api.resources.tutorials import tutorials
-from api.resources.faq import faq
-from api.resources.events import events
-from api.resources.mentors import mentors
 from api.resources.courses import courses
+from api.resources.events import events
+from api.resources.faq import faq
+from api.resources.mentors import mentors
+from api.resources.tutorials import tutorials
 
+MONGO_HOST = os.environ["MONGO_HOST"]
 MONGO_USERNAME = os.environ["MONGO_USERNAME"]
 MONGO_PASSWORD = os.environ["MONGO_PASSWORD"]
 MONGO_DBNAME = os.environ["MONGO_DBNAME"]
